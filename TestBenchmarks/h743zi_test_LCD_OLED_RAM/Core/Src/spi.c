72636de8947ba5ebd66d4b8b5ec9b3309a234c12
RATelectro <rom.ted@mail.ru>
2025-10-16 14:21:32 +0300

    add basic LCD and DRAM test

---
+++ TestBenchmarks/h743zi_test_LCD_OLED_RAM/Core/Src/spi.c
@@ -0,0 +1,190 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    spi.c
+  * @brief   This file provides code for the configuration
+  *          of the SPI instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "spi.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+SPI_HandleTypeDef hspi5;
+DMA_HandleTypeDef hdma_spi5_rx;
+DMA_HandleTypeDef hdma_spi5_tx;
+
+/* SPI5 init function */
+void MX_SPI5_Init(void)
+{
+
+  /* USER CODE BEGIN SPI5_Init 0 */
+
+  /* USER CODE END SPI5_Init 0 */
+
+  /* USER CODE BEGIN SPI5_Init 1 */
+
+  /* USER CODE END SPI5_Init 1 */
+  hspi5.Instance = SPI5;
+  hspi5.Init.Mode = SPI_MODE_MASTER;
+  hspi5.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi5.Init.DataSize = SPI_DATASIZE_4BIT;
+  hspi5.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi5.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi5.Init.NSS = SPI_NSS_HARD_OUTPUT;
+  hspi5.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+  hspi5.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi5.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi5.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi5.Init.CRCPolynomial = 0x0;
+  hspi5.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  hspi5.Init.NSSPolarity = SPI_NSS_POLARITY_LOW;
+  hspi5.Init.FifoThreshold = SPI_FIFO_THRESHOLD_01DATA;
+  hspi5.Init.TxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN;
+  hspi5.Init.RxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN;
+  hspi5.Init.MasterSSIdleness = SPI_MASTER_SS_IDLENESS_00CYCLE;
+  hspi5.Init.MasterInterDataIdleness = SPI_MASTER_INTERDATA_IDLENESS_00CYCLE;
+  hspi5.Init.MasterReceiverAutoSusp = SPI_MASTER_RX_AUTOSUSP_DISABLE;
+  hspi5.Init.MasterKeepIOState = SPI_MASTER_KEEP_IO_STATE_DISABLE;
+  hspi5.Init.IOSwap = SPI_IO_SWAP_DISABLE;
+  if (HAL_SPI_Init(&hspi5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI5_Init 2 */
+
+  /* USER CODE END SPI5_Init 2 */
+
+}
+
+void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  if(spiHandle->Instance==SPI5)
+  {
+  /* USER CODE BEGIN SPI5_MspInit 0 */
+
+  /* USER CODE END SPI5_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI5;
+    PeriphClkInitStruct.Spi45ClockSelection = RCC_SPI45CLKSOURCE_HSI;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* SPI5 clock enable */
+    __HAL_RCC_SPI5_CLK_ENABLE();
+
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+    /**SPI5 GPIO Configuration
+    PF6     ------> SPI5_NSS
+    PF7     ------> SPI5_SCK
+    PF8     ------> SPI5_MISO
+    PF9     ------> SPI5_MOSI
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF5_SPI5;
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    /* SPI5 DMA Init */
+    /* SPI5_RX Init */
+    hdma_spi5_rx.Instance = DMA1_Stream1;
+    hdma_spi5_rx.Init.Request = DMA_REQUEST_SPI5_RX;
+    hdma_spi5_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_spi5_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_spi5_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_spi5_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_spi5_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_spi5_rx.Init.Mode = DMA_NORMAL;
+    hdma_spi5_rx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_spi5_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_spi5_rx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(spiHandle,hdmarx,hdma_spi5_rx);
+
+    /* SPI5_TX Init */
+    hdma_spi5_tx.Instance = DMA1_Stream2;
+    hdma_spi5_tx.Init.Request = DMA_REQUEST_SPI5_TX;
+    hdma_spi5_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_spi5_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_spi5_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_spi5_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_spi5_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_spi5_tx.Init.Mode = DMA_NORMAL;
+    hdma_spi5_tx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_spi5_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_spi5_tx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(spiHandle,hdmatx,hdma_spi5_tx);
+
+    /* SPI5 interrupt Init */
+    HAL_NVIC_SetPriority(SPI5_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(SPI5_IRQn);
+  /* USER CODE BEGIN SPI5_MspInit 1 */
+
+  /* USER CODE END SPI5_MspInit 1 */
+  }
+}
+
+void HAL_SPI_MspDeInit(SPI_HandleTypeDef* spiHandle)
+{
+
+  if(spiHandle->Instance==SPI5)
+  {
+  /* USER CODE BEGIN SPI5_MspDeInit 0 */
+
+  /* USER CODE END SPI5_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SPI5_CLK_DISABLE();
+
+    /**SPI5 GPIO Configuration
+    PF6     ------> SPI5_NSS
+    PF7     ------> SPI5_SCK
+    PF8     ------> SPI5_MISO
+    PF9     ------> SPI5_MOSI
+    */
+    HAL_GPIO_DeInit(GPIOF, GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9);
+
+    /* SPI5 DMA DeInit */
+    HAL_DMA_DeInit(spiHandle->hdmarx);
+    HAL_DMA_DeInit(spiHandle->hdmatx);
+
+    /* SPI5 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(SPI5_IRQn);
+  /* USER CODE BEGIN SPI5_MspDeInit 1 */
+
+  /* USER CODE END SPI5_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
---
+++ TestBenchmarks/h743zi_test_LCD_OLED_RAM/Drivers/W9812G6JH/w9812g6jh.h
@@ -0,0 +1,96 @@
+/*
+ * w9812g6jh.h
+ *
+ *  Created on: 30 ???. 2020 ?.
+ *      Author: Predtech
+ */
+
+#ifndef INC_W9812G6JH_H_
+#define INC_W9812G6JH_H_
+
+/*
+ * ======== SDRAM initialization ======================
+
+The initialization sequence is managed by software. If the two banks are used, the
+initialization sequence must be generated simultaneously to Bank 1and Bank 2 by setting
+the Target Bank bits CTB1 and CTB2 in the FMC_SDCMR register:
+
+1. Program the memory device features into the FMC_SDCRx register. The SDRAM
+clock frequency, RBURST and RPIPE must be programmed in the FMC_SDCR1
+register.
+
+2. Program the memory device timing into the FMC_SDTRx register. The TRP and TRC
+timings must be programmed in the FMC_SDTR1 register.
+
+3. Set MODE bits to ‘001’ and configure the Target Bank bits (CTB1 and/or CTB2) in the
+FMC_SDCMR register to start delivering the clock to the memory (SDCKE is driven
+high).
+4. Wait during the prescribed delay period. Typical delay is around 100 μs (refer to the
+SDRAM datasheet for the required delay after power-up).
+
+5. Set MODE bits to ‘010’ and configure the Target Bank bits (CTB1 and/or CTB2) in the
+FMC_SDCMR register to issue a “Precharge All” command.
+
+6. Set MODE bits to ‘011’, and configure the Target Bank bits (CTB1 and/or CTB2) as well
+as the number of consecutive Auto-refresh commands (NRFS) in the FMC_SDCMR
+register. Refer to the SDRAM datasheet for the number of Auto-refresh commands that
+should be issued. Typical number is 8.
+
+7. Configure the MRD field, set the MODE bits to ‘100’, and configure the Target Bank bits
+(CTB1 and/or CTB2) in the FMC_SDCMR register to issue a “Load Mode Register”
+command and program the SDRAM device. In particular the Burst Length (BL) has to
+be set to ‘1’) and the CAS latency has to be selected. If the Mode Register is not the
+same for both SDRAM banks, this step has to be repeated twice, once for each bank
+and the Target Bank bits set accordingly. For mobile SDRAM devices, the MRD field is
+also used to configure the extended mode register while issuing the Load Mode
+Register”
+
+8. Program the refresh rate in the FMC_SDRTR register
+The refresh rate corresponds to the delay between refresh cycles. Its value must be
+adapted to SDRAM devices.
+At this stage the SDRAM device is ready to accept commands. If a system reset occurs
+during an ongoing SDRAM access, the data bus might still be driven by the SDRAM device.
+Therefore the SDRAM device must be first reinitialized after reset before issuing any new
+access by the NOR Flash/PSRAM/SRAM or NAND Flash controller.
+
+*Note: If two SDRAM devices are connected to the FMC, all the accesses performed at the same
+time to both devices by the Command Mode register (Load Mode Register command) are
+issued using the timing parameters configured for SDRAM Bank 1 (TMRD andTRAS
+timings) in the FMC_SDTR1 register.
+ */
+
+#include "main.h"
+
+#define SDRAM_TIMEOUT     ((uint16_t)0xFFFF)
+
+#define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
+#define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
+#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
+#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
+#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
+#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
+#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
+
+#define SDRAM_BANK_ADDR                 ((uint32_t)0xC0000000)
+#define SDRAM_RFR_COUNT					3744 // clock cycles to refresh
+
+#define BUFFER_SIZE         ((uint32_t)0x1000000) // 16M bits
+#define WRITE_READ_ADDR     ((uint32_t)0x0000000)
+
+
+// write and read single u32 macro
+#define _RAM_WRITE32(data, addr)   ( *(__IO uint32_t*) (SDRAM_BANK_ADDR + addr) = data )
+#define _RAM_READ32(addr)          ( *(__IO uint32_t*) (SDRAM_BANK_ADDR + addr)        )
+// examples:
+// _RAM_WRITE32(u32 a = 0xFFFFFFFF, 0 )
+// u32 a = _RAM_READ(0);
+// calculation example:
+// http://main.lv/writeup/stm32f4_sdram_configuration.md
+
+void W9812G6JH_Init(SDRAM_HandleTypeDef *hsdram);
+
+#endif /* INC_W9812G6JH_H_ */
